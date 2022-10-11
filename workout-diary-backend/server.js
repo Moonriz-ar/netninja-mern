2e387397bf2a4e28cf7df4c3b2b9e8fcfbf5d9ae
Andrea Lin <andre93.lin@gmail.com>
2022-10-11 15:22:11 +0800

    Add db connection and api endpoints

--- workout-diary-backend/server.js
+++ workout-diary-backend/server.js
@@ -1,17 +1,36 @@
 require('dotenv').config();
 
 const express = require('express');
+const mongoose = require('mongoose');
+
+const workoutRoutes = require('./routes/workouts');
+
 const port = process.env.PORT;
 
 // instanciate express app
 const app = express();
 
-// routes
-app.get('/', (req, res) => {
-  res.json({ mssg: 'Welcome to the app' });
+// middleware to parse json
+app.use(express.json());
+
+// middleware to log the requests
+app.use((req, res, next) => {
+  console.log(req.path, req.method);
+  next();
 });
 
-// listen for requests
-app.listen(port, () => {
-  console.log(`listening on port ${port}`);
-});
+// routes
+app.use('/api/workouts', workoutRoutes);
+
+// connect to db
+mongoose
+  .connect(process.env.MONGO_URI)
+  .then(() => {
+    // listen for requests
+    app.listen(port, () => {
+      console.log(`connected to db and listening on port ${port}`);
+    });
+  })
+  .catch((error) => {
+    console.log(error);
+  });
